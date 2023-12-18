7b8bebe01bb0b18272f513921f5273a99299c4aa
lontubu <nickson.lontubu@studentmoringa.com>
2023-12-18 14:10:49 +0300

    assighnment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = (parseInt(Math.random()) +1);
+
+const num3 = 11;
+const num4 = 7;
+const mod = num3 % num4;
+
+
+const max = 20
